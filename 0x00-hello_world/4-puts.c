8268a49599e8b889af14dcd163f2263deb2fbe00
Nouran <nourankaram2005@gmail.com>
2023-09-07 19:45:02 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void)
-{puts("/"Programming is like building a multilingual puzzle");
+{
+	/**
+	 * main - this is the main function
+	 *
+	 * Return: always 0
+	 */
+puts("/"Programming is like building a multilingual puzzle");
 
-return (0);}
+return (0);
+
+}
